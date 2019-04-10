4dcb6b793a8d7f70831a66a59e05560cfef8d543
Stanislav <ostapenkostas@gmail.com>
2019-04-10 10:40:32 +0300

    h

---
+++ data/SchoolsofBibrsk_0.js
@@ -0,0 +1,14 @@
+var json_SchoolsofBibrsk_0 = {
+"type": "FeatureCollection",
+"name": "SchoolsofBibrsk_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME_SCH": "НВК \"ЗОШ - ДНЗ\"", "NASEL_PUN": "Романів", "STUPIN_SKH": "I-III" }, "geometry": { "type": "Point", "coordinates": [ 24.3431238, 49.7068599 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "Свірзька ЗОШ", "NASEL_PUN": "Свірж", "STUPIN_SKH": "I-III" }, "geometry": { "type": "Point", "coordinates": [ 24.4360219, 49.6481292 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "НВК \"ЗОШ ДНЗ\"", "NASEL_PUN": "Лани", "STUPIN_SKH": "I-II" }, "geometry": { "type": "Point", "coordinates": [ 24.2923964, 49.6028095 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "Глібовицька ЗОШ", "NASEL_PUN": "Глібовичі", "STUPIN_SKH": "I-II" }, "geometry": { "type": "Point", "coordinates": [ 24.435309, 49.6239038 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "ЗОШ", "NASEL_PUN": "Бібрка", "STUPIN_SKH": "I-III" }, "geometry": { "type": "Point", "coordinates": [ 24.296732831078454, 49.640250038738522 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "НВК \"ЗОШ ДНЗ\"", "NASEL_PUN": "Стоки", "STUPIN_SKH": "I-II" }, "geometry": { "type": "Point", "coordinates": [ 24.3640479, 49.629699 ] } },
+{ "type": "Feature", "properties": { "NAME_SCH": "ЗОШ", "NASEL_PUN": "Великі Глібовичі", "STUPIN_SKH": "I-III" }, "geometry": { "type": "Point", "coordinates": [ 24.2317301, 49.6145162 ] } }
+]
+}
